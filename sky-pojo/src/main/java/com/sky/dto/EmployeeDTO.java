9a58f32ba57c277e80d5675de08919c1a11b0b1d
komorebi6666 <1733308612@qq.com>
2025-07-18 11:54:56 +0800

    苍穹外卖_新增员工

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,25 @@
 package com.sky.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@Api(tags = "新增员工时传递的数据模型")
 public class EmployeeDTO implements Serializable {
-
+    @ApiModelProperty("员工id")
     private Long id;
-
+    @ApiModelProperty("员工用户名")
     private String username;
-
+    @ApiModelProperty("员工姓名")
     private String name;
-
+    @ApiModelProperty("手机号")
     private String phone;
-
+    @ApiModelProperty("员工密码")
     private String sex;
-
+    @ApiModelProperty("身份证号")
     private String idNumber;
 
 }
